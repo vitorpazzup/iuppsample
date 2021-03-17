ab739bb796f4ad8f0891355e0e87ddca0ad8b9f5
Nilton Guilhem Junior <njguilhem@gmail.com>
2021-03-17 00:40:30 -0300

    teste

--- src/main/kotlin/com/itau/service/impl/ProductServiceImpl.kt
+++ src/main/kotlin/com/itau/service/impl/ProductServiceImpl.kt
@@ -7,12 +7,17 @@ import com.itau.model.EventsInformationDto
 import com.itau.model.Product
 import com.itau.model.dto.ProductDto
 import com.itau.service.ProductService
+import io.nats.client.AuthHandler
+import io.nats.client.Connection
+import io.nats.client.Nats
+import io.nats.client.Options
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import java.nio.charset.StandardCharsets
 import javax.inject.Singleton
 
 @Singleton
-class ProductServiceImpl(private val client: ProductClient): ProductService {
+class ProductServiceImpl: ProductService {
 
     val LOG : Logger = LoggerFactory.getLogger(ProductExceptionHandler::class.java)
 
@@ -21,7 +26,10 @@ class ProductServiceImpl(private val client: ProductClient): ProductService {
             Events.SAVE_PRODUCT,
             Product(1L,product.name,product.price)
         )
-        client.send(eventsInformation)
+        val natsConnect: Connection = Nats
+            .connect("nats://dark-lord:qUzfnpj2JudVcuqYpuTihRJE@localhost:4222")
+        natsConnect.publish("my-products","teste".toByteArray(StandardCharsets.UTF_8))
+        natsConnect.close()
         LOG.info("message successfully sent to the broker {}", eventsInformation.events.event)
         return product
     }
