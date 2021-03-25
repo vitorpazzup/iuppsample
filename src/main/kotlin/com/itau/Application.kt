57c48a0c7bc19ed4ab79757d1b6f3e7950acabc8
Nilton Guilhem Junior <njguilhem@gmail.com>
2021-03-25 12:29:05 -0300

    remove deduce environment

--- src/main/kotlin/com/itau/Application.kt
+++ src/main/kotlin/com/itau/Application.kt
@@ -1,10 +1,14 @@
 package com.itau
 
+import io.micronaut.context.ApplicationContext
 import io.micronaut.runtime.Micronaut.*
 fun main(args: Array<String>) {
+	ApplicationContext.run()
 	build()
 	    .args(*args)
 		.packages("com.itau")
+		.deduceEnvironment(false)
+		.banner(false)
 		.start()
 }
 
