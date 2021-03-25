e5393d61cc87e05f6ea9188c7c556794bedacf8f
Nilton Guilhem Junior <njguilhem@gmail.com>
2021-03-25 14:54:48 -0300

    exclude envs

--- src/main/kotlin/com/itau/Application.kt
+++ src/main/kotlin/com/itau/Application.kt
@@ -1,13 +1,14 @@
 package com.itau
 
 import io.micronaut.context.ApplicationContext
+import io.micronaut.context.env.Environment
 import io.micronaut.runtime.Micronaut.*
 fun main(args: Array<String>) {
 	ApplicationContext.run()
 	build()
 	    .args(*args)
 		.packages("com.itau")
-		.environmentVariableExcludes("k8s")
+		.environmentVariableExcludes(Environment.KUBERNETES,Environment.AMAZON_EC2,Environment.CLOUD)
 		.environments("dev")
 		.start()
 }
