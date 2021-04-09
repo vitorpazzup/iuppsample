71f79f181098af898babc73744e92475bde5260d
Nilton Guilhem Junior <njguilhem@gmail.com>
2021-04-09 18:05:27 -0300

    include swagger

--- src/main/kotlin/com/itau/Application.kt
+++ src/main/kotlin/com/itau/Application.kt
@@ -3,6 +3,22 @@ package com.itau
 import io.micronaut.context.ApplicationContext
 import io.micronaut.context.env.Environment
 import io.micronaut.runtime.Micronaut.*
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Contact
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.info.License
+
+@OpenAPIDefinition(
+	info = Info(
+		title = "IUPP Product",
+		version = "0.1",
+		description = "IUPP Sample",
+		license = License(name = "Apache 2.0", url = "https://foo.bar"),
+		contact = Contact(url = "https://www.iupp.com.br/", name = "IUPP", email = "IUPP@IUPP.com")
+	)
+)
+object Api {
+}
 fun main(args: Array<String>) {
 	ApplicationContext.run()
 	build()
